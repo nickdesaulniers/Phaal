88d31737969f610879afa72a116cc6003467281e
Nick Desaulniers <ndesaulniers@mozilla.com>
2012-09-03 20:40:27 -0400

    initialized a new rails dir

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phaal::Application.config.secret_token = '8f7df6d1b26f4a1ee8734f24a0bede54640ed6a0d46368252972c4baf0d13b7bf2ed01cf9daa01096a4079122660333d257a7bd35eb52d74f8f877f71ab9cdc4'
